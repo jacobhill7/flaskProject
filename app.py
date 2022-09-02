732d628c657c1a23c5e0aba8881951afe1e96118
Jacob Hill <9932336@schoolsnet.act.edu.au>
2022-09-02 11:48:56 +1000

    app.py update

--- app.py
+++ app.py
@@ -3,7 +3,16 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
+from models import Contact
+from forms import ContactForm
+
+@app.route("/contact.html", methods=["POST", "GET"])
+def contact():
+    form = ContactForm()
+    return render_template("contact.html", title ="Contact Us", form=form)
 
 @app.route('/')
 def hello_world():  # put application's code here
