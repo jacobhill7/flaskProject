e1c2b7514a61a7347caac0be897e6c61d98c614c
Jacob Hill <9932336@schoolsnet.act.edu.au>
2022-09-02 11:54:08 +1000

    app.py update 2

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ from forms import ContactForm
 @app.route("/contact.html", methods=["POST", "GET"])
 def contact():
     form = ContactForm()
+    if form.validate_on_submit():
+        new_contact = Contact(name=form.name.data, email=form.email.data, message=form.message.data)
+        db.session.add(new_contact)
+        db.session.commit()
     return render_template("contact.html", title ="Contact Us", form=form)
 
 @app.route('/')
